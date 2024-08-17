c5b7fb4461c4781b98f2a5e9fd1e8f3dc71d5eaf
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-17 22:52:06 +0300

    events js ana sayfadaki selektörler tanımlandı

---
+++ src/events.js
@@ -0,0 +1,15 @@
+//*CATEGORY BUTTONS
+const allBtn = document.querySelector(".btn-primary");
+const electronicsBtn = document.querySelector(".btn-secondary");
+const sportsBtn = document.querySelector(".btn-success");
+const homeBtn = document.querySelector(".btn-danger");
+const shopBtn = document.querySelector(".btn-warning");
+const clothingBtn = document.querySelector(".btn-info");
+
+//* SEARCH BUTTON
+const searchInput = document.querySelector("#searchInput");
+
+//* TEXT CHANGE
+const categoryDisplay = document.querySelector(".categoryDisplay");
+
+
