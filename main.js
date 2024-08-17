dae4e563eace40b432674dc7834bf42dcfb0e73e
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-18 00:19:15 +0300

    electronics kategori butonu eklendi

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 import "./scss/style.scss";
 
-// import { search } from "./src/display";
+//*SELEKTÖRLER
+const electronicsBtn = document.querySelector(".electronicsBtn");
 
 export const urunleriGetir = async (query) => {
   const url = `https://anthonyfs.pythonanywhere.com/api/products/`;
 
   try {
     const res = await axios(url);
-    console.log(res.data);
     res.data.forEach((item) => {
       document.querySelector(".mainRow").innerHTML += `
     <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
@@ -32,14 +32,40 @@ export const urunleriGetir = async (query) => {
             </div>
           </div>
         </div>
-    
-    
-    
     `;
+    });
+    electronicsBtn.addEventListener("click", () => {
+      document.querySelector(".mainRow").textContent = "";
+      res.data.forEach((item) => {
+        if (item.category_id == 1) {
+          document.querySelector(".mainRow").innerHTML += `
+    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+          <div class="card">
+            <img src="${item.image}" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+              <p class="card-text">
+                ${item.description}
+              </p>
+              <div class="lastDiv">
+                <div class="price">
+                  <h5>Fiyat:</h5>
+                  <h5>${item.price} $</h5>
+                </div>
+                <div class="priceBtn">
+                  <button type="button" class="btn btn-danger">Sepete Ekle</button>
+                  <button type="button" class="btn btn-primary">Detay</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    `;
+        }
+      });
     });
   } catch (error) {
     console.log(error);
   }
 };
 urunleriGetir();
-console.log("mardatone");
--- src/display.js
+++ src/display.js
@@ -1,7 +1,2 @@
-// import { urunleriGetir } from "main.js";
-// import { searchInput, categoryDisplay } from "./events";
 
-// export const search = () => {
-//   const searchVal = searchInput.value;
-//   urunleriGetir(searchVal);
-// };
+
