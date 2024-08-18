c87d2368e9a7a878860121b9e84d383c720ec465
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-18 21:22:37 +0300

    evets.js silindi

--- src/events.js
+++ src/events.js
@@ -1,13 +1,1 @@
-//*CATEGORY BUTTONS
-export const allBtn = document.querySelector(".btn-primary");
-export const electronicsBtn = document.querySelector(".btn-secondary");
-export const sportsBtn = document.querySelector(".btn-success");
-export const homeBtn = document.querySelector(".btn-danger");
-export const shopBtn = document.querySelector(".btn-warning");
-export const clothingBtn = document.querySelector(".btn-info");
 
-//* SEARCH BUTTON
-export const searchInput = document.querySelector("#searchInput");
-
-//* TEXT CHANGE
-export const categoryDisplay = document.querySelector(".categoryDisplay");
